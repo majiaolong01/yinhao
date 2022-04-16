dbfb3bea6d629c5ed13582e96f46ee6ab07502a9
majiaolong01 <majiaolong0105@sina.com>
2022-04-16 10:42:31 +0800

    删除无用的文件......

--- src/redux/reducers.js
+++
@@ -1,4 +0,0 @@
-//合并reducer并且返回
-import {combineReducers } from 'redux'
-import user from './login.reducer'
-export default combineReducers ({user});
--- src/service/proxyAddress.js
+++
@@ -1,8 +0,0 @@
-
-const proxyAddress=[
-    {
-        context:'/api/',
-        target:'http://127.0.0.1:8080/',
-    }
-    ]
-module.exports= proxyAddress
